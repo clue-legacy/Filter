5b6830e36157512865c50f4391d7f8bb5dcd575f
Christian Lück <christian@lueck.tv>
2011-05-27 13:00:47 +0200

    Implement Filter_Interface_Negate

--- Filter/Fail.php
+++ Filter/Fail.php
@@ -1,7 +1,11 @@
 <?php
 
-class Filter_Fail extends Filter{
+class Filter_Fail extends Filter implements Filter_Interface_Negate{
     public function toSql($db){
         return '0';
+    }
+    
+    public function toNegate(){
+        return Filter::success();
     }
 }
--- Filter/Success.php
+++ Filter/Success.php
@@ -1,7 +1,11 @@
 <?php
 
-class Filter_Success extends Filter{
+class Filter_Success extends Filter implements Filter_Interface_Negate{
     public function toSql($db){
         return '1';
+    }
+    
+    public function toNegate(){
+        return Filter::fail();
     }
 }
